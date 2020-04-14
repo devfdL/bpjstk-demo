ff443e7abb67ae0166abf01c6d0220d1e3d5c273
Muhammad Fadhil <33900154+devfdL@users.noreply.github.com>
2020-04-14 20:46:52 +0700

    Update settings.py

--- demo/settings.py
+++ demo/settings.py
@@ -111,6 +111,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+DISABLE_COLLECTSTATIC=1
+
 # Add these new lines
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
